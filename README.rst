c3580ca90b76eedf374a9b4c7c98e971ff8a76b8
= <=>
2019-10-22 23:11:34 -0400

    Release

--- README.rst
+++ README.rst
@@ -8,15 +8,15 @@ Description
 
 This web-project will be builded using this stack:
 
-1. Bootstrap
+1. Bootstrap, jsGrid
 
 2. Flask Microframework (python)
 
 3. DynamoDB
 
 
-The finally of this "internal" web-page is for inform to the users of 
-the bot, what are the activities related over the using it.
+The finally of this "internal" web-page is inform to the users of 
+the bot, what are the activities related about their using.
 
 ======================
 Public Subdomain
@@ -28,9 +28,10 @@ Public Subdomain
 Instructions for Run the server
 ================================
 
-0. CLone the repo. 
+0. Clone the repo. 
 
     $ git clone git@github.com:realFranco/Bots-Actions.git
+    $ git clone https://github.com/realFranco/Bots-Actions.git
 
 1. Install the dependecies. (Previusly you need to have 
     python3 installed on your system.)
@@ -41,25 +42,83 @@ Instructions for Run the server
 
     $ python3 app.py
 
-2.1 If you need to close the terminal where the script it is running,
-    do this.
+2.1 If you need to close the terminal where the script it is running, do this.
 
     $ nohup python3 app.py &
-    This will output a numbers, that becomes in the pID of the script.
 
-    Actually you can find that process using this command:
+This will output a numbers, that becomes in the pID of the script.
+
+Actually you can find that process using this command:
 
     $ ps -fa | grep python
 
-    And kill it using:
+And kill it using:
 
-    $ kill -9 pID (where pID is the number returned on after 
-    nohup or the ps command).
+    $ kill -9 pID 
+    
+Where pID is the number returned on after nohup or the ps command.
 
-===========================================================
-If your pourpose it is run this under a subdomain, do this:
-===========================================================
+=================================================================
+If your pourpose it is to host this web app into an ec2 instance
+=================================================================
 
 0. Step initial
+    
+    Open the port 80 & 443 for transfer data from the instance to the public requests.
+    Initially, using just $ nohup sudo python3 app.py & can serve the app
 
-    $ nohup python3 app.py &
+1. Install this packeages
+   
+   $ sudo apt-get install nginx
+   $ sudo ap-get install gunicorn3
+
+1. Inside to the ec2 instance write this lines, and edit/create son configuration files, for run unicon like a service
+
+    $ cd /etc/systemd/system/
+    $ sudo nano gunicorn3.service
+
+    [Unit]
+    Description=Gunicorn service
+    After=network.target
+    
+    [Service]
+    User=ubuntu
+    Group=www-data
+    WorkingDirectory=/home/ubuntu/bots/Bots-Actions
+    ExecStart=/usr/bin/gunicorn3 --workers 1 --bind unix:flaskapp.sock -m 007 app:app    
+    
+    # Reload the deamon
+    $ sudo systemctl daemon-reload
+
+    # Start the service
+    $ sudo service gunicorn3 start
+
+    # Check the service
+    $ sudo service gunicorn3 status
+
+    $ cd /etc/nginx/sites-enabled/
+
+    # Write the configuration file for the server
+    $ sudo nano flaskapp
+    
+    server{
+        listen 80;
+        server_name 52.23.187.181; # or the public ipv4 that you have for the instance
+
+        location / {
+            proxy_pass http://unix:/home/ubuntu/bots/Bots-Actions/flaskapp.sock;
+        } 
+    }
+
+    $ sudo service nginx restart
+    $ sudo service gunicorn3 restart
+    
+For more information, follow this videos series:
+    https://www.youtube.com/channel/UCwDlyuX3Fkg5WNBufLnH6dw
+
+================================
+Add a subdomain in a Hosted Zone
+================================
+
+Edit the A record (under the DNS Management, for example, Route53 on was) with the name of the subdomain and a the ip public of the instance that serve the flask app
+
---
+++ static/scripts/utils.js
@@ -0,0 +1,394 @@
+/* Designed on October 10th, 2019
+First Release on October 22th, 2019
+
+Dev: franco@systemagency.com
+
+This piece of software will contain many of the routines need it by
+the whole internal site for System Agency (botsactions.systemagency.com)
+
+If some issue detected, plese report it to the Developer.
+*/
+
+const domain = "http://0.0.0.0:5000/";
+
+async function getCountries(selectorOfTheGrid, country){
+
+    var staticRoute = domain + "queryItems?country=" + country
+
+    const response = await fetch(staticRoute);
+    var staticData = await response.json();
+
+    var endPoint = "";
+
+    function editParams(domain, endPoint, filter){
+
+        let params = "";
+        let goTo = "";
+
+        for(var key in filter){
+            item = filter[key]
+            if (item != "")  params += `${key}=${item}&`
+        }
+        params = params.slice(0,-1);
+        goTo = domain + endPoint + params;
+
+        return {
+            "goTo": goTo,
+            "filter": filter
+        };
+    }
+
+    $(selectorOfTheGrid).jsGrid({    
+        
+        controller: {
+            
+            loadData : (filter) => {
+                // filter, dictionary with params    
+                endPoint = "queryItems?";
+                filter["country"] = country;
+
+                _args = editParams(domain, endPoint, filter)
+
+                console.log(_args["goTo"])
+                console.log(_args["filter"])
+
+                return $.ajax({
+                    type:"GET",
+                    url: _args["goTo"],
+                    data: _args["filter"]
+                });
+
+            },
+
+            insertItem: $.noop,
+            
+            deleteItem : $.noop,
+
+            updateItem : $.noop,
+        },
+
+        width: "100%",
+        height: "auto", /*400px*/
+        
+        filtering: true,
+        inserting: false,
+        editing: false,
+        sorting: true,
+        paging: true,
+        
+        pageSize: 10,
+        pageButtonCount: 5,
+
+        data: staticData,
+
+        fields: [
+            { 
+                name:"image", title: "Image", type: "text", width: 100, 
+                visible: false,
+                cellRenderer : (value, item) => {
+                    let img = ` <td align="center"> 
+                                    <img src ="${value}" width="50"> 
+                                </td>`
+                    return img;
+                }
+            },
+            { name:"url_name", title: "Agency Name", type: "text", width: 150},
+            { name:"date_in", title: "GSI", type: "Date", width: 150, visible:false},
+            { name:"country", title: "Country", type: "text", width: 150, visible:false},
+            { name:"gsidataportion", title: "GSI", type: "text", width: 150, visible:false},
+            { name: "pkurl", title:"Url", type: "text", width: 250, validate: "required", editing:false }
+        ]
+    });
+
+} // => End of getCountries
+
+async function log_in(){
+    var goTo = domain.concat("login");
+    var email = document
+                    .getElementById("email")
+                    .value
+                    .split("@")[0]
+                    .concat("@systemagency.com"),
+                password = document
+                            .getElementById("password")
+                            .value;
+
+    var SHA256 = new Hashes.SHA256
+    password = SHA256.hex(password)
+
+    await $.ajax({
+        type:"POST",
+        url: goTo,
+        headers: {
+            "Email": email,
+            "Password": password
+        }
+    });
+    window.location.replace(domain);
+}
+
+async function getCountries_auth(selectorOfTheGrid, country){
+    var staticRoute = domain + "queryItems?country=" + country
+
+    const response = await fetch(staticRoute);
+    var staticData = await response.json();
+
+    var endPoint = "";
+
+    function editParams(domain, endPoint, filter){
+
+        let params = "";
+        let goTo = "";
+
+        for(var key in filter){
+            item = filter[key]
+            if (item != "")  params += `${key}=${item}&`
+        }
+        params = params.slice(0,-1);
+        goTo = domain + endPoint + params;
+
+        return {
+            "goTo": goTo,
+            "filter": filter
+        };
+    }
+
+    $(selectorOfTheGrid).jsGrid({    
+        
+        controller: {
+            
+            loadData : (filter) => {
+                // filter, dictionary with params    
+                endPoint = "queryItems?";
+                filter["country"] = country;
+
+                _args = editParams(domain, endPoint, filter)
+
+                console.log(_args["goTo"])
+                console.log(_args["filter"])
+
+                return $.ajax({
+                    type:"GET",
+                    url: _args["goTo"],
+                    data: _args["filter"]
+                });
+
+            },
+
+            insertItem: async function insertI(filter){
+                endPoint = "createItem?";
+                
+                filter["country"] = country;
+                filter["gsidataportion"] = "agency";
+                filter["progress"] = "In Progress";
+                
+                _args = editParams(domain, endPoint, filter)
+
+                console.log(_args["goTo"])
+                console.log(_args["filter"])
+
+                await  $.ajax({
+                    type:"POST",
+                    url: _args["goTo"],
+                    data: _args["filter"]
+                });
+            },
+            
+            deleteItem : (filter) =>{
+                endPoint = "deleteItem?";
+
+                _args = editParams(domain, endPoint, filter)
+
+                console.log(_args["goTo"])
+                console.log(_args["filter"])
+
+                $.ajax({
+                    type:"DELETE",
+                    url: _args["goTo"],
+                    data: _args["filter"]
+                });
+            },
+
+            updateItem : (filter) => {
+                endPoint = "updateItem?";
+                
+                _args = editParams(domain, endPoint, filter)
+
+                console.log(_args["goTo"])
+                console.log(_args["filter"])
+
+                $.ajax({
+                    type:"PUT",
+                    url: _args["goTo"],
+                    data: _args["filter"]
+                });
+            },
+        },
+
+        width: "100%",
+        height: "auto", /*400px*/
+        
+        filtering: true,
+        inserting: true,
+        editing: true,
+        sorting: true,
+        paging: true,
+        
+        pageSize: 10,
+        pageButtonCount: 5,
+
+        data: staticData,
+
+        fields: [
+            { 
+                name:"image", title: "Image", type: "text", width: 100, 
+                visible: false,
+                cellRenderer : (value, item) => {
+                    let img = ` <td align="center"> 
+                                    <img src ="${value}" width="50"> 
+                                </td>`
+                    return img;
+                }
+            },
+            { name:"url_name", title: "Agency Name", type: "text", width: 150},
+            { name:"date_in", title: "GSI", type: "Date", width: 150, visible:false},
+            { name:"country", title: "Country", type: "text", width: 150, visible:false},
+            { name:"gsidataportion", title: "GSI", type: "text", width: 150, visible:false},
+            { name: "pkurl", title:"Url", type: "text", width: 250, validate: "required", editing:false },
+            { 
+                name: "progress", title:"Progress", type: "text", width: 70, validate: "required", editing:false, inserting:false,
+                cellRenderer : (value, item) => {
+                    let l_progreess = "warning", tag_progress = '<td align="center"> </td>';
+                    
+                    if (value){
+                        if (value == "Completed") 
+                            l_progreess = "success";
+                            
+                        tag_progress = `
+                                        <td align="center">
+                                            <span class="badge badge-pill badge-${l_progreess}">
+                                                ${value}
+                                            </span>
+                                        </td>`;
+                    }
+                        
+                  return tag_progress;
+                }},
+            { type: "control" }
+        ]
+    });
+
+} // => End of getCountries
+
+async function log_out(){
+    console.log("log_out");
+
+    var goTo = domain + "logout";
+
+    await $.ajax({
+        type:"GET",
+        url: goTo
+    });
+
+    window.location.replace(domain);
+}
+
+function new_country(){
+    let add_country = document.getElementById('newCountry').value,
+        find = "heading"+add_country, _continue = true;
+    
+    if(add_country != '' && document.getElementById(find) == undefined){
+        country_body = `<div class="card">
+            <div class="card-header"  id="heading${add_country}">
+                <h2 class="mb-0">
+                    <button 
+                        class="btn btn-link collapsed" 
+                        type="button" 
+                        data-toggle="collapse" 
+                        data-target="#collapse${add_country}" 
+                        aria-expanded="false" 
+                        aria-controls="collapse${add_country}" 
+                        onclick="getCountries_auth('#jsGrid${add_country}', '${add_country}')">
+                        ${add_country}
+                    </button>
+                    <button type="button" class=" float-right btn btn-warning" onclick="delete_card('heading${add_country}')">Delete</button>
+                </h2>
+            </div>
+                <div id="collapse${add_country}" class="collapse" aria-labelledby="heading${add_country}" data-parent="#accordionCountry">
+                    <div id="jsGrid${add_country}"></div>
+                </div>
+            </div>            
+        </div>`
+
+        document.getElementById('accordionCountry').innerHTML += country_body;
+    }
+    else{
+        // Repeated element or empty 
+        console.log("It is not possible to add a new country.");
+    }
+}
+
+async function delete_card(target){
+    let goTo = domain.concat('deleteCountry?', 'country=', target.replace('heading', ''));
+    document.getElementById(target).remove();
+    
+    await $.ajax({
+        type:"DELETE",
+        url: goTo
+    });
+}
+
+async function sing_in(){
+    var goTo = domain.concat("registerUser");
+    var email = document
+                    .getElementById("email")
+                    .value
+                    .split("@")[0]
+                    .concat("@systemagency.com"),
+                password = document
+                            .getElementById("password")
+                            .value;
+
+    var SHA256 =  new Hashes.SHA256
+    password = SHA256.hex(password)
+
+    out = $.ajax({
+        type:"PUT",
+        url: goTo
+        , headers: {
+            "Email": email,
+            "Password": password
+        }
+    });   
+    await out;
+    
+    window.location.replace(domain);
+}
+
+async function reset_pass(){
+    let email = document.getElementById("email").value;
+    let newPass = document.getElementById("password").value;
+
+    if (email.length > 0){
+        if (newPass.length > 0){
+            let SHA256 =  new Hashes.SHA256
+            newPass = SHA256.hex(newPass)  
+            
+            console.log('goog password')
+            console.log(newPass)
+            let endPoint = "updatePass?";
+            let goTo = domain + endPoint.concat(
+                    'email=', email, '@systemagency.com',
+                    '&password=', newPass);
+
+            console.log(goTo)
+
+            await $.ajax({
+                type:"PUT",
+                url: goTo
+            });
+
+            window.location.replace(domain);
+        }
+    }
+}
